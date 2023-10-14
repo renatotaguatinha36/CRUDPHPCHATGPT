9cc5fea49849eaf5fe8f6e825399789abbb7e8fb
renatoguara2020 <renatoguara2020@gmail.com>
2023-10-14 10:20:38 -0300

    ok

--- add.php
+++ add.php
@@ -23,15 +23,19 @@ include("db.php");
 <html>
 <head>
     <title>Add Employee</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
 <body>
     <h1>Add Employee</h1>
     <form method="POST" action="add.php">
-        <label>First Name: <input type="text" name="first_name" required></label>
-        <label>Last Name: <input type="text" name="last_name" required></label>
-        <label>Email: <input type="email" name="email" required></label>
-        <label>Job Title: <input type="text" name="job_title" required></label>
+    <div class="form-group">  
+        <label>First Name: <input type="text" name="first_name" required class="form-control"></label>
+        <label>Last Name: <input type="text" name="last_name" required class="form-control"></label>
+        <label>Email: <input type="email" name="email" required class="form-control"></label>
+        <label>Job Title: <input type="text" name="job_title" required class="form-control"></label>
         <button type="submit">Add</button>
+    </div>    
     </form>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>        
 </body>
 </html>
